38f134e32e9e6caaecd9ed052f4936f41599ff23
PabloBrodovski <135301252+PabloBrodovski@users.noreply.github.com>
2023-09-27 00:00:10 -0300

    Agrego los archivos js

---
+++ js/producto.js
@@ -0,0 +1,26 @@
+const destacado = JSON.parse(localStorage.getItem("producto"));
+
+const mostrarProducto = document.querySelector("#producto-elegido");
+ 
+destacado.forEach((product) => {
+    let content = document.createElement("div");
+    content.className = "card2";
+    content.innerHTML = `
+        <div>
+            <h3 class="name">${product.nombre}</h3> </div>
+        <div class="imagen-descripcion">
+            <img class="imagen" src="${product.imagen}">
+            <p class="descriptionLarge">${product.descripcionLarga}</p>
+        </div>
+        <div>
+            <p class="price-product">$ ${product.precio}</p>
+        </div>
+        <div>
+            ${product.estrellas}
+        </div>
+    `;
+
+    mostrarProducto.append(content);
+});
+
+localStorage.removeItem(productoElegido);
